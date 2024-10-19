4ae26537bdd89c1eb91916e6dcf3915353274405
Vighnesh  Kandalgaonkar <78204375+Vighnesh2304@users.noreply.github.com>
2024-10-19 19:52:38 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # Vighnesh2304
    # Input:
    # p, principal amount
    # t, time period in years
